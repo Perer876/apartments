b16dfc1c3f45682d216eab7515b8e982a664ca81
Oscar Arambula <oscar.arambula4388@alumnos.udg.mx>
2022-03-18 19:14:12 -0600

    Se muestran los edificios en el index

--- resources/views/buildings/index.blade.php
+++ resources/views/buildings/index.blade.php
@@ -12,26 +12,32 @@
                     </div>
                     <div class="card-body">
                         <div class="buttons">
-                            <a href="#" class="btn btn-primary">Nuevo edificio</a>
+                            <a href="/buildings/create" class="btn btn-primary">Nuevo edificio</a>
                         </div>
                     </div>
                 </div>
             </div>
+            @foreach ($buildings as $building)
             <div class="col-xl-4 col-md-6 col-sm-12">
                 <div class="card">
                     <div class="card-content">
                         <div class="card-body">
-                            <h4 class="card-title">Alias</h4>
+                            <h4 class="card-title">{{$building->alias}}</h4>
                             <p class="card-text">
-                                Calle No.
+                                {{$building->street . ' ' . $building->number}}
+                                <br>
+                                {{$building->city . ', ' . $building->state}}
+                                <br>
+                                {{$building->postcode}}
                             </p>
                         </div>
                     </div>
                     <div class="card-footer d-flex justify-content-between">
-                        <a class="btn btn-light-primary" href="#">Detalle</a>
+                        <a class="btn btn-info" href="#">Detalle</a>
                     </div>
                 </div>
             </div>
+            @endforeach
         </section>
     </div>    
 </x-base>
