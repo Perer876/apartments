be3ff8772fa01d44f57aa0dbc3908debc2de6701
Oscar Arambula <oscar.arambula4388@alumnos.udg.mx>
2022-03-25 19:26:48 -0600

    Sidebar-item se muestra activo también en subdirectorios

--- resources/views/components/sidebar-item.blade.php
+++ resources/views/components/sidebar-item.blade.php
@@ -1,7 +1,6 @@
-<li
-class="sidebar-item {{ Request::is($route) ? 'active' : '' }}">
-<a href="{{ $href }}" class='sidebar-link'>
-    <i class="bi bi-{{ $icon }}-fill"></i>
-    <span>{{ $slot }}</span>
-</a>
+<li class="sidebar-item {{ Request::segment(1) == $route ? 'active' : '' }}">
+    <a href="{{ $href }}" class='sidebar-link'>
+        <i class="bi bi-{{ $icon }}-fill"></i>
+        <span>{{ $slot }}</span>
+    </a>
 </li>
